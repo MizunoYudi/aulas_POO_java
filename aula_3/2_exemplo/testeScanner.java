0e73cac41e4fb42a34a889f09d582929113146d5
MizunoYudi <joaomizunovy@gmail.com>
2025-08-28 20:27:22 -0300

    Terceira Aula

---
+++ aula_3/2_exemplo/testeScanner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class testeScanner {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.err.println("Digite alguma coisa");
+		while(sc.hasNext()){
+			String value = sc.next();
+			if(value.equals("Mexido")){
+				break;
+			}
+			System.out.println(sc.next());
+		}
+		sc.close();
+	}
+}
