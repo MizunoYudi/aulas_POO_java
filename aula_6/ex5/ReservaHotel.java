bb8d176b99c2ea2f586a21dc6ddeb936f462e450
MizunoYudi <joaomizunovy@gmail.com>
2025-09-18 20:34:36 -0300

    Inicio do ex5 aula 6

---
+++ aula_6/ex5/ReservaHotel.java
@@ -0,0 +1,11 @@
+public class ReservaHotel{
+    public static ReservaHotel[] = reservaHotel[];
+    private String nome;
+    private int noites;
+    private int pessoas;
+    private boolean cafeIncluso;
+
+    public reservar(){
+
+    }
+}
