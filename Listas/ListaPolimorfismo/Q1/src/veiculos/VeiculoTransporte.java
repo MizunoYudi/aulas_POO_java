f47b87e6b320513bee54223f85dc62822d3786af
MizunoYudi <joaomizunovy@gmail.com>
2025-10-30 19:59:11 -0300

    ListaPolimorfismo - Criando VeiculoTransporte

---
+++ Listas/ListaPolimorfismo/Q1/src/veiculos/VeiculoTransporte.java
@@ -0,0 +1,14 @@
+package veiculos;
+
+public abstract class VeiculoTransporte {
+	public int capacidade;
+	public String identificador;
+	
+	public void mover() {
+		
+	}
+	
+	public void mostrarInfo() {
+		
+	}
+}
